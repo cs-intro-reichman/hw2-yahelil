9389bbd03a70339ed462b078b74b07f04be889a7
yahelil <yahelil.levity@gmail.com>
2024-11-15 00:00:12 +0200

    fixing a,an

--- Cheers.java
+++ Cheers.java
@@ -5,14 +5,15 @@ public class Cheers {
             int number_of_cheers = Integer.parseInt(args[1]);
             String lettersForAn = "AEFHILMNORSX";
             String an_a;
-            if (lettersForAn.indexOf(cheer_name.charAt(0)) != -1){
-                an_a = "a";
-            }
-            else{
-                an_a = "an";
-            }
+            
             for (int index = 0; index < cheer_name.length(); index++) {
                 char c = cheer_name.charAt(index);
+                if (lettersForAn.indexOf(c) != -1){
+                        an_a = "an";
+                    }
+                    else{
+                        an_a = "a";
+                    }
                 System.out.println("Give me " + an_a + ' ' + c + ": " + c + '!');
             }
             System.out.println("What does that spell?");
